4d45906e3d34fd276794ef677435f47473b88920
Rwandevops <47103610+Rwandevops@users.noreply.github.com>
2019-11-20 13:51:07 +0100

    Update SQLQueriesUser.php
    nom de fonction en camel case
    séparation update en nameUpdate / emailUpdate / passwordUpdate / isAdminUpdate

--- SQLQueriesUser.php
+++ SQLQueriesUser.php
@@ -2,11 +2,11 @@
 //user : id, username, password, email, admin
 
 // Add user :
-function add_user(PDO $db)
+function userAdd(PDO $db)
 {
-    $sql = "INSERT INTO users (username, password, email, admin VALUES(:username, :password, :email, :admin)";
+    $sql = "INSERT INTO users (username, password, email, admin VALUES(:username, :password, :email, :admin))";
                                           
-    $stmt = $pdo->prepare($sql);
+    $stmt = $db->prepare($sql);
                                               
     $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
     $stmt->bindParam(':password', $_POST['password'], PDO::PARAM_STR); 
@@ -14,22 +14,60 @@ function add_user(PDO $db)
     $stmt->bindParam(':admin', $_POST['admin'], PDO::PARAM_INT); 
                                       
     $stmt->execute();
-
 }
 
 // ------------------------------------------------------------------------------------------------------------ //
 
-// Update user :
-function update_user(PDO $db)
+// Update user name:
+function userNameUpdate(PDO $db)
 {
-    $sql = "UPDATE users SET username = :username, password = :password, email = :email, admin = :admin WHERE id = :id";
+    $sql = "UPDATE users SET username = :username WHERE id = :id";
 
-    $stmt = $pdo->prepare($sql);                                  
+    $stmt = $db->prepare($sql);                                  
     
     $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
-    $stmt->bindParam(':password', $_POST['password'], PDO::PARAM_STR); 
-    $stmt->bindParam(':email', $_POST['email'], PDO::PARAM_STR);       
-    $stmt->bindParam(':admin', $_POST['admin'], PDO::PARAM_INT);  
+   
+    $stmt->execute();
+}
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+// Update user email:
+function userEmailUpdate(PDO $db)
+{
+    $sql = "UPDATE users SET email = :email WHERE id = :id";
+
+    $stmt = $db->prepare($sql);                                  
+    
+    $stmt->bindParam(':username', $_POST['email'], PDO::PARAM_STR);
+   
+    $stmt->execute();
+}
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+// Update user name:
+function userPasswordUpdate(PDO $password)
+{
+    $sql = "UPDATE users SET password = :password WHERE id = :id";
+
+    $stmt = $db->prepare($sql);                                  
+    
+    $stmt->bindParam(':username', $_POST['password'], PDO::PARAM_STR);
+   
+    $stmt->execute();
+}
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+// Update user name:
+function userIsAdminUpdate(PDO $db)
+{
+    $sql = "UPDATE users SET isAdmin = :isAdmin WHERE id = :id";
+
+    $stmt = $db->prepare($sql);                                  
+    
+    $stmt->bindParam(':username', $_POST['isAdmin'], PDO::PARAM_STR);
    
     $stmt->execute();
 }
@@ -37,11 +75,11 @@ function update_user(PDO $db)
 // ------------------------------------------------------------------------------------------------------------ //
 
 // Delete user :
-function delete_user(PDO $db)
+function userDelete(PDO $db)
 {
     $sql = "DELETE FROM users WHERE id =  :id";
     
-    $stmt = $pdo->prepare($sql);
+    $stmt = $db->prepare($sql);
     
     $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_INT);  
 
