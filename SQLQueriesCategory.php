700274c28e03a1658dc3e95d92e33652dfd7bd5a
Rwandevops <47103610+Rwandevops@users.noreply.github.com>
2019-11-20 13:34:21 +0100

    Update SQLQueriesCategory.php
    Séparation update en updateName et updateParentId
    noms de fonction en camel case

--- SQLQueriesCategory.php
+++ SQLQueriesCategory.php
@@ -2,12 +2,12 @@
 //category : id, name, parent_id
 
 // Add category :
-function add_category(PDO $db)
+function categoryAdd(PDO $db)
 {
   
-    $sql = "INSERT INTO categories (name, parent_id VALUES(:name, :parent_id)";
+    $sql = "INSERT INTO categories (name, parent_id VALUES(:name, :parent_id))";
                                           
-    $stmt = $pdo->prepare($sql);
+    $stmt = $db->prepare($sql);
                                               
     $stmt->bindParam(':name', $_POST['name'], PDO::PARAM_STR);       
     $stmt->bindParam(':parent_id', $_POST['parent_id'], PDO::PARAM_INT); 
@@ -17,27 +17,36 @@ function add_category(PDO $db)
 
 // ------------------------------------------------------------------------------------------------------------ //
 
-// Update category :
-function update_category(PDO $db)
+// Update category name:
+function categoryUpdateName(PDO $db)
 {
-    $sql = "UPDATE categories SET name = :name, parent_id = :parent_id  WHERE id = :id";
+    $sql = "UPDATE categories SET name = :name WHERE id = :id";
 
-    $stmt = $pdo->prepare($sql);                                  
-    
-    $stmt->bindParam(':name', $_POST['name'], PDO::PARAM_STR);       
-    $stmt->bindParam(':parent_id', $_POST['parent_id'], PDO::PARAM_INT);    
-   
+    $stmt = $db->prepare($sql);                                  
+    $stmt->bindParam(':name', $_POST['name'], PDO::PARAM_STR);           
+    $stmt->execute();
+}
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+// Update category parent_id:
+function categoryUpdateParentId(PDO $db)
+{
+    $sql = "UPDATE categories SET parent_id = :parent_id WHERE id = :id";
+
+    $stmt = $db->prepare($sql);                                  
+    $stmt->bindParam(':parent_id', $_POST['parent_id'], PDO::PARAM_STR);           
     $stmt->execute();
 }
 
 // ------------------------------------------------------------------------------------------------------------ //
 
 // Delete category :
-function delete_category(PDO $db)
+function categoryDelete(PDO $db)
 {
     $sql = "DELETE FROM categories WHERE id =  :id";
     
-    $stmt = $pdo->prepare($sql);
+    $stmt = $db->prepare($sql);
     
     $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_INT);  
 
