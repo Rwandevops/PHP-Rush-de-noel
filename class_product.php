df145eb77eb34b11354da82741f604d17659f137
Xhann <55038465+Xhann@users.noreply.github.com>
2019-11-20 11:26:16 +0100

    Update class_product.php

--- class_product.php
+++ class_product.php
@@ -59,38 +59,3 @@ class product
             //détruire la ligne product dans la BDD
         }
 }
-
-class category
-{
-    private $name;
-    protected $parentId;
-    static $ID = 0;
-
-    public function setName($name)
-        {
-            $this->name = $name;
-        }
-
-    public function getName()
-        {
-            return $this->name;
-        }
-
-    public function getParentId()
-        {
-            return $this->parentId;
-        }
-    
-        public function __construct($name, $parent_id)
-        {
-            setName($name);
-            $this->parentId = $parent_id;
-            self::$ID++;
-            // + enregistrement dans la BDD
-        }
-
-    public function __destruct()
-        {
-            //détruire la ligne product dans la BDD
-        }
-}
