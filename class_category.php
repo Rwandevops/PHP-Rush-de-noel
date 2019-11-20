dc0661fb0e3b4d248dbad53ad802053efa08cffc
Rwandevops <47103610+Rwandevops@users.noreply.github.com>
2019-11-20 11:07:51 +0100

    Add files via upload
    séparation classes en 1 classe par fichier

---
+++ class_category.php
@@ -0,0 +1,34 @@
+class category
+{
+    private $name;
+    protected $parentId;
+    static $ID = 0;
+
+    public function setName($name)
+        {
+            $this->name = $name;
+        }
+
+    public function getName()
+        {
+            return $this->name;
+        }
+
+    public function getParentId()
+        {
+            return $this->parentId;
+        }
+    
+        public function __construct($name, $parent_id)
+        {
+            setName($name);
+            $this->parentId = $parent_id;
+            self::$ID++;
+            // + enregistrement dans la BDD
+        }
+
+    public function __destruct()
+        {
+            //détruire la ligne product dans la BDD
+        }
+}
