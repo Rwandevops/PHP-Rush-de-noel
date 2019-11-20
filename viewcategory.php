cb5f1a052197bef01ccb618114bbafab7f3b34e7
Xhann <55038465+Xhann@users.noreply.github.com>
2019-11-20 11:19:44 +0100

    Add files via upload

---
+++ viewcategory.php
@@ -0,0 +1,33 @@
+<?php
+class Viewuser extends User
+{
+    public function showAllUsers()
+    {
+        $datas=$this->getAllUsers();
+        foreach ($datas as $data)
+        {
+            echo $data['id']."<br>";
+            echo $data['username']."<br>";
+            echo $data['password']."<br>";
+            echo $data['email']."<br>";
+            echo $data['admin']."<br>";
+            
+        }
+        
+    }
+    public function showSelfUser()
+    {
+        $datas=$this->getSelfUser();
+        foreach ($datas as $data)
+        {
+            echo $data['id']."<br>";
+            echo $data['username']."<br>";
+            echo $data['password']."<br>";
+            echo $data['email']."<br>";
+            echo $data['admin']."<br>";
+            
+        }
+        
+    }
+
+}
