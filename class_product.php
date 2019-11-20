5e044a915975c49ce967d106d23735fb1c424258
Rwandevops <47103610+Rwandevops@users.noreply.github.com>
2019-11-20 18:11:15 +0100

    Update class_product.php

--- class_product.php
+++ class_product.php
@@ -7,49 +7,52 @@ class Product extends Database
     private $name;
     private $category;
     private $price;
-    private static $ID = 0;
+    private static $Id = 0;
 
-    public function getName()
+    public function getName($conn)
         {
-            return $this->name;
+            return SQLGetProductName($conn, $this->Id);
         }
 
-    public function setName($name)
+    public function setName($conn, $name)
         {
             $this->name = $name;
+            SQLUpdateProductName($conn, $this->Id, $name);
         }
 
     public function getCategory()
         {
-            return $this->category;
+            return SQLGetProductCategory($conn, $this->Id);
         }
 
     public function setCategory($category)
         {
             $this->category = $category;
+            SQLUpdateProductCategory$conn, $this->Id, $category);
         }
     
     public function getPrice()
         {
-            return $this->price;
+            return SQLGetProductPrice($conn, $this->Id);
         }
 
     public function setPrice($price)
         {
             $this->price = $price;
+            SQLUpdateProductPrice($conn, $this->Id, $price);
         }
 
     public function __construct($name, $category, $price)
         {
-            setName($name);
-            setCategory($category);
-            setPrice($price);
-            self::$ID++;
-            add_product($conn); // enregistrement dans la BDD
+            $this->name = $name;
+            $this->category = $category;
+            $this->price = $price;
+            self::$Id++;
+            addProduct($conn, $name, $category, $price); // enregistrement dans la BDD
         }
 
-    public function __destruct()
+    public function __destruct($conn)
         {
-            delete_product($conn); //détruire la ligne product dans la BDD
+            delete_product($conn, $this->Id); //détruire la ligne product dans la BDD
         }
 }
